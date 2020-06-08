168a604707613fe1665ff44ec8efc8b44cbaef3b
osama radwan <OsamaR@leading-point.com>
2020-06-08 12:58:48 +0300

    Test Commit After Release

--- VSTestProject/Controllers/HomeController.cs
+++ VSTestProject/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace VSTestProject.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View("About");
         }
 
         public ActionResult About()
